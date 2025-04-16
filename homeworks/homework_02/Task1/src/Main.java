8fc7597b756a1a90eae999fb78aad63f266307ad
StanislavVidulin <stas_vidulin@icloud.com>
2025-04-16 15:39:37 +0200

    add homework02

---
+++ homeworks/homework_02/Task1/src/Main.java
@@ -0,0 +1,27 @@
+public class Main {
+    public static void main(String[] args) {
+//        Задача №1
+//
+//        Реализовать 2 потока:
+//        первый поток должен выводить на экран все числа, которые делятся на 2;
+//        второй поток должен выводить все числа которые делятся на 3;
+//        Main должен запускать оба эти потока, засыпать на 3 секунды и завершать выполнение программы
+//              (т.е. оба потока тоже должны прекратить свою работу)
+
+        System.out.println("Main start");
+
+        Thread thread1 = new MyThread();
+        Thread thread2 = new Thread(new MyRunnable());
+        thread1.start();
+        thread2.start();
+
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        System.out.println("Main finish");
+
+    }
+}
