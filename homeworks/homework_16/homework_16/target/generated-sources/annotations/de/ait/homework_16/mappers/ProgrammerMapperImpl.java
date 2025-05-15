554737625445da7865d011d0f801c43a7cfba5ac
StanislavVidulin <stas_vidulin@icloud.com>
2025-05-15 11:21:42 +0200

    fix

---
+++ homeworks/homework_16/homework_16/target/generated-sources/annotations/de/ait/homework_16/mappers/ProgrammerMapperImpl.java
@@ -0,0 +1,59 @@
+package de.ait.homework_16.mappers;
+
+import de.ait.homework_16.dto.ProgrammerDto;
+import de.ait.homework_16.model.Programmer;
+import java.util.ArrayList;
+import java.util.List;
+import javax.annotation.processing.Generated;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2025-05-15T09:44:56+0200",
+    comments = "version: 1.6.3, compiler: javac, environment: Java 17.0.13 (Amazon.com Inc.)"
+)
+@Component
+public class ProgrammerMapperImpl implements ProgrammerMapper {
+
+    @Override
+    public Programmer toProgrammer(ProgrammerDto dto) {
+        if ( dto == null ) {
+            return null;
+        }
+
+        Programmer programmer = new Programmer();
+
+        programmer.setName( dto.getName() );
+
+        return programmer;
+    }
+
+    @Override
+    public ProgrammerDto toProgrammerDto(Programmer programmer) {
+        if ( programmer == null ) {
+            return null;
+        }
+
+        String name = null;
+
+        name = programmer.getName();
+
+        ProgrammerDto programmerDto = new ProgrammerDto( name );
+
+        return programmerDto;
+    }
+
+    @Override
+    public List<ProgrammerDto> toListProgrammerDto(List<Programmer> programmers) {
+        if ( programmers == null ) {
+            return null;
+        }
+
+        List<ProgrammerDto> list = new ArrayList<ProgrammerDto>( programmers.size() );
+        for ( Programmer programmer : programmers ) {
+            list.add( toProgrammerDto( programmer ) );
+        }
+
+        return list;
+    }
+}
---
+++ homeworks/homework_16/homework_16/target/generated-sources/annotations/de/ait/homework_16/mappers/TaskMapperImpl.java
@@ -0,0 +1,59 @@
+package de.ait.homework_16.mappers;
+
+import de.ait.homework_16.dto.TaskDto;
+import de.ait.homework_16.model.Task;
+import java.util.ArrayList;
+import java.util.List;
+import javax.annotation.processing.Generated;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2025-05-15T09:44:56+0200",
+    comments = "version: 1.6.3, compiler: javac, environment: Java 17.0.13 (Amazon.com Inc.)"
+)
+@Component
+public class TaskMapperImpl implements TaskMapper {
+
+    @Override
+    public List<TaskDto> toListTaskDto(List<Task> tasks) {
+        if ( tasks == null ) {
+            return null;
+        }
+
+        List<TaskDto> list = new ArrayList<TaskDto>( tasks.size() );
+        for ( Task task : tasks ) {
+            list.add( toTaskDto( task ) );
+        }
+
+        return list;
+    }
+
+    @Override
+    public TaskDto toTaskDto(Task task) {
+        if ( task == null ) {
+            return null;
+        }
+
+        TaskDto taskDto = new TaskDto();
+
+        taskDto.setDescription( task.getDescription() );
+        taskDto.setPriority( task.getPriority() );
+
+        return taskDto;
+    }
+
+    @Override
+    public Task toTask(TaskDto dto) {
+        if ( dto == null ) {
+            return null;
+        }
+
+        Task task = new Task();
+
+        task.setDescription( dto.getDescription() );
+        task.setPriority( dto.getPriority() );
+
+        return task;
+    }
+}
